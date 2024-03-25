3461dc0cb0a99d147e5441e62bcc1e402e516340
Francois <11577022+takenagain@users.noreply.github.com>
2024-03-25 19:00:27 +0200

    Fix fvm sudo install issue

--- flutter.sh
+++ flutter.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-apt install -y curl git unzip xz-utils zip libglu1-mesa \
+sudo apt install -y curl git unzip xz-utils zip libglu1-mesa \
 	clang cmake git \
+      python3 python3-pip python3-venv \
       ninja-build pkg-config \
       libgtk-3-dev liblzma-dev \
       libstdc++-12-dev \
       libc6:i386 libncurses5:i386 \
     libstdc++6:i386 lib32z1 \
-    libbz2-1.0:i386 \
-    python3 python3-pip python3-venv
+    libbz2-1.0:i386 
     
 curl -fsSL https://fvm.app/install.sh | bash
 
 fvm install 3.16.9
 fvm install stable
 fvm install 2.8.1
-
 
 echo "export PATH=\"/home/frannas/.fvm_flutter/bin:$PATH\"" >> ~/.bashrc
 echo "alias flutter=\"fvm flutter\"" >> ~/.bashrc
--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+# [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 timedatectl set-local-rtc 1 --adjust-system-clock
 timedatectl
 
 ./flatpak.sh
+./ssh.sh
 ./podman_docker.sh
 ./rocm.sh
 ./flutter.sh
+
+docker compose -f rocm-compose.yml up -d
+docker compose -f rustdesk-compose.yml up -d
+
+./modular.sh
