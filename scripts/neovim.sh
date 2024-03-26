86e758d0278070ccba0e016468ccc18ae6172887
Francois <11577022+takenagain@users.noreply.github.com>
2024-03-26 07:11:19 +0200

    Fix missing neovim dependency

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -2,7 +2,7 @@ sudo apt-get install -y software-properties-common
 sudo apt-get install -y python-software-properties
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install -y neovim
-sudo apt-get install -y python-dev python-pip python3-dev python3-pip
+sudo apt-get install -y neovim fd-find
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip 
 
 git clone git@github.com:takenagain/nvchad-config.git ~/.config/nvim
