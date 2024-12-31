7afa9dc32adfdac1ae472e88af098f8509af3797
Francois <11577022+takenagain@users.noreply.github.com>
2024-12-31 07:23:32 +0200

    fix yubikey-agent install

--- scripts/debian/git.sh
+++ scripts/debian/git.sh
@@ -1,6 +1,35 @@
-sudo apt install libpam-u2f yubikey-agent
+sudo apt install libpam-u2f yubikey-agent pinentry-qt pcscd
 export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/yubikey-agent/yubikey-agent.sock"
 echo "export SSH_AUTH_SOCK=\"${XDG_RUNTIME_DIR}/yubikey-agent/yubikey-agent.sock\"" | tee -a ~/.bashrc
+
+echo "[Unit]
+Description=Seamless ssh-agent for YubiKeys
+Documentation=https://filippo.io/yubikey-agent
+
+[Service]
+ExecStart=/usr/bin/yubikey-agent -l %t/yubikey-agent/yubikey-agent.sock
+ExecReload=/bin/kill -HUP $MAINPID
+IPAddressDeny=any
+RestrictAddressFamilies=AF_UNIX
+RestrictNamespaces=yes
+RestrictRealtime=yes
+RestrictSUIDSGID=yes
+LockPersonality=yes
+SystemCallFilter=@system-service
+SystemCallFilter=~@privileged @resources
+SystemCallErrorNumber=EPERM
+SystemCallArchitectures=native
+NoNewPrivileges=yes
+KeyringMode=private
+UMask=0177
+RuntimeDirectory=yubikey-agent
+
+[Install]
+WantedBy=default.target" | sudo tee /etc/systemd/system/yubikey-agent.service
+
+systemctl daemon-reload --user
+sudo systemctl enable --now pcscd.socket
+systemctl --user enable --now yubikey-agent
 
 # Restore the SSH credentials fro the security key
 cd ~/.ssh
